378b8d434ea5fbe75bba87b1bab8ec7f4ac417e2
Elias-Rivera-U <stefano.elias.r.u.06@gmail.com>
2025-02-03 14:56:59 -0500

    Implementa una función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 //Arreglo amigos
 let amigos = [];
+
+
+function agregarAmigo() {
+    let nombre_amigo = document.getElementById("amigo").value;     // Capturar el valor de entrada
+    
+    if (nombre_amigo == "") {       //Validar la entrada
+        alert("Por favor, inserte un nombre.")
+    } else {
+        amigos.push(nombre_amigo); //Actualizar el array de amigos
+        limpiar_entrada()//Limpiar el campo de entrada
+        return nombre_amigo;
+        
+    }
+}
+
+
+function limpiar_entrada(){
+    document.getElementById("amigo").value = "";
+}
