f197d3da7fe9bdb9f598d855726fa2be92c305ea
Elias-Rivera-U <stefano.elias.r.u.06@gmail.com>
2025-02-04 12:47:40 -0500

    Implementa una función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -9,9 +9,22 @@ function agregarAmigo() {
         alert("Por favor, inserte un nombre.")
     } else {
         amigos.push(nombre_amigo); //Actualizar el array de amigos
+        actualizarLista()
         limpiar_entrada()//Limpiar el campo de entrada
         return nombre_amigo;
         
+    }
+}
+
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos"); // Obtener el elemento de la lista
+    lista.innerHTML = ""; // Limpiar la lista antes de agregar nuevos elementos
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li"); // Crear un nuevo elemento <li>
+        li.textContent = amigos[i]; // Asignar el nombre del amigo al <li>
+        lista.appendChild(li); // Agregar el <li> a la lista
     }
 }
 
